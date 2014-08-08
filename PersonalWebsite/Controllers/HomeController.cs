4ff53da92ecb6be4acd30def4ff1fa243293fd80
tjm585 <tjm585@gmail.com>
2014-08-07 17:41:47 -0700

    added profile pic and cleand up some code and files.

--- PersonalWebsite/Controllers/HomeController.cs
+++ PersonalWebsite/Controllers/HomeController.cs
@@ -6,25 +6,33 @@ using System.Web.Mvc;
 
 namespace PersonalWebsite.Controllers
 {
+    /// <summary>
+    /// Main controller.
+    /// </summary>
     public class HomeController : Controller
     {
+        /// <summary>
+        /// The index (default) page for the website.
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
+        /// <summary>
+        /// An action that displays the contact page.
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Contact()
         {
             return View();
         }
 
+        /// <summary>
+        /// An action that displays the changelist.
+        /// </summary>
+        /// <returns></returns>
         public ActionResult ChangeList()
         {
             return View();
